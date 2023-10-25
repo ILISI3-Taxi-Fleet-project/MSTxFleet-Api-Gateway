83ddbcfc9756dcf9cf67ac65132c8c7884e33d53
musdev <w0l1d.dev@gmail.com>
2023-10-25 21:46:56 +0100

    Initial commit

---
+++ src/test/java/com/ilisi/mstxfleetapigateway/MsTxFleetApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTxFleetApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
